28cc421389f5ff2c8b5658a735a369226df60690
Ramonog9 <ramonog9@gmail.com>
2025-09-28 12:06:59 -0500

    Adding timing

--- guessingGame.py
+++ guessingGame.py
@@ -1,9 +1,13 @@
 import random
 import time
 
-guess = int(input("Welcome to the Guessing Game! You have 3 attempts to guess the correct number between 1 and 10. Press any number to start!"))
+guess = int(input("Welcome to the Guessing Game! You have 3 attempts to guess the correct number between 1 and 10. Press 0 to start!"))
 guesses_left = 3
+time.sleep(3)
+print("Choosing a number!")
 correct_number = random.randint(1,10)
+time.sleep(2)
+print("I got my number! Bet you can't guess it!")
 
 while guess != correct_number and guesses_left > 0:
     guess = int(input("What is your guess?"))
@@ -12,7 +16,7 @@ while guess != correct_number and guesses_left > 0:
     else:
         print("Guess higher")
     guesses_left -= 1
-if guesses_left == 0:
+if guesses_left == 0 and guess != correct_number:
     print("You're out of guesses! The correct number was:", correct_number)
 else:
     print("You guessed correct!")
