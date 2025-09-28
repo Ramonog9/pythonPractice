d94784c9a0dfa5f69092aa6c1eda4be423a56d09
Ramonog9 <ramonog9@gmail.com>
2025-09-28 11:59:27 -0500

    Adding max attempts to guess

--- guessingGame.py
+++ guessingGame.py
@@ -1,12 +1,18 @@
 import random
+import time
 
-guess = int(input("Guess a number between 1 and 10?"))
+guess = int(input("Welcome to the Guessing Game! You have 3 attempts to guess the correct number between 1 and 10. Press any number to start!"))
+guesses_left = 3
 correct_number = random.randint(1,10)
 
-while guess != correct_number:
+while guess != correct_number and guesses_left > 0:
     guess = int(input("What is your guess?"))
     if guess > correct_number:
         print("Guess lower")
     else:
         print("Guess higher")
-print("You guessed correct!")
+    guesses_left -= 1
+if guesses_left == 0:
+    print("You're out of guesses! The correct number was:", correct_number)
+else:
+    print("You guessed correct!")
